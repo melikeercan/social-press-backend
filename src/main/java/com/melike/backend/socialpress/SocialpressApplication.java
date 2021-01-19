19eb3cff540cde61ec8268ac46450dcd4766ae73
Melike Ercan <melike.ercan.metu@gmail.com>
2021-01-19 12:24:02 +0100

    backend init

---
+++ src/main/java/com/melike/backend/socialpress/SocialpressApplication.java
@@ -0,0 +1,13 @@
+package com.melike.backend.socialpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melike/backend/socialpress/SocialpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melike.backend.socialpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
