5388f0696c1efc2dff6092675e8cd2d93be176fb
Melike Ercan <melike.ercan.metu@gmail.com>
2021-01-22 12:40:40 +0100

    serializable

--- src/main/java/com/melike/backend/socialpress/dto/YoutubeTrend.java
+++ src/main/java/com/melike/backend/socialpress/dto/YoutubeTrend.java
@@ -1,6 +1,12 @@
 package com.melike.backend.socialpress.dto;
 
-public class YoutubeTrend {
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import java.io.Serializable;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class YoutubeTrend implements Serializable {
     private final String id;
     private final String title;
 
