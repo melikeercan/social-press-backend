aec475863f5efb7e4c90755c32c55069d148422c
Melike Ercan <melike.ercan.metu@gmail.com>
2021-01-22 20:06:23 +0100

    update

--- src/main/java/com/melike/backend/socialpress/response/YoutubeRelatedVideosQueryResult.java
+++ src/main/java/com/melike/backend/socialpress/response/YoutubeRelatedVideosQueryResult.java
@@ -7,9 +7,15 @@ import java.util.List;
 public class YoutubeRelatedVideosQueryResult {
     private List<YoutubeResult> youtubeResults;
 
-    private YoutubeRelatedVideosQueryResult() {}
-
     public YoutubeRelatedVideosQueryResult(List<YoutubeResult> youtubeResults) {
+        this.youtubeResults = youtubeResults;
+    }
+
+    public List<YoutubeResult> getYoutubeResults() {
+        return youtubeResults;
+    }
+
+    public void setYoutubeResults(List<YoutubeResult> youtubeResults) {
         this.youtubeResults = youtubeResults;
     }
 }
